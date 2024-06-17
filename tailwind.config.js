3ef51b831cde4599b2b9c24eed677bec6da4bb03
nazi <nazipaeez777@gmail.com>
2024-06-17 20:54:52 +0330

    Init project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        "poppins-black": ["poppins-black"],
+        "poppins-bold": ["poppins-bold"],
+        "poppins-xBold": ["poppins-xBold"],
+        "poppins-light": ["poppins-light"],
+        "poppins-xLight": ["poppins-xLight"],
+        "poppins-medium": ["poppins-medium"],
+        "poppins-regular": ["poppins-regular"],
+        "poppins-semiBold": ["poppins-semiBold"],
+        "poppins-thin": ["poppins-thin"],
       },
     },
   },
   plugins: [],
-}
+};
