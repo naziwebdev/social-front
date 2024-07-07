8f37599a2f3345f0ff4a29b9bf7e6e743bfd3c56
nazi <nazipaeez777@gmail.com>
2024-07-08 00:22:21 +0330

    Develope login validator

---
+++ src/validations/login.js
@@ -0,0 +1,16 @@
+import * as yup from "yup";
+
+const loginValidator = yup.object().shape({
+  email: yup.string().email("ایمیل را به درستی وارد کنید"),
+  password: yup
+    .string()
+    .required("رمز عبور خود را وارد نمایید")
+    .min(6, "رمز عبور باید حداقل ۶ کارکتر باشد")
+    .max(20, "رمز عبور باید حذاکثر ۲۰ کارکتر باشد")
+    .matches(
+      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/,
+      "رمز عبور باید حاوی حروف بزرگ و کوچک و اعداد و علائم باشد"
+    ),
+});
+
+export default loginValidator;
